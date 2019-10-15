3cec133c1b4da5d8514e6d54b1fc538860bf5a41
Rahil <rahilmerchant99@gmail.com>
2019-10-15 17:18:04 +0530

    bottom nav working

--- app/src/main/java/com/example/lifeline/LeaderboardActivity.java
+++ app/src/main/java/com/example/lifeline/LeaderboardActivity.java
@@ -1,11 +1,17 @@
 package com.example.lifeline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -25,7 +31,33 @@ public class LeaderboardActivity extends AppCompatActivity {
         setTitle("Leaderboard");
         userRef=db.collection("users");
         setUpRecyclerView();
+
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+        Menu menu =bottomNav.getMenu();
+        MenuItem menuItem=menu.getItem(3);
+        menuItem.setChecked(true);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_profile:
+                    startActivity(new Intent(LeaderboardActivity.this,homeActivity.class));
+                    break;
+                case R.id.navigation_appointment:
+                    break;
+                case R.id.navigation_events:
+                    break;
+                case R.id.navigation_leaderboard:
+                    startActivity(new Intent(LeaderboardActivity.this,LeaderboardActivity.class));
+                    break;
+
+            }
+            return true;
+        }
+    };
 
     private void setUpRecyclerView() {
         Query query = userRef.orderBy("timesDonated", Query.Direction.DESCENDING)
--- app/src/main/java/com/example/lifeline/homeActivity.java
+++ app/src/main/java/com/example/lifeline/homeActivity.java
@@ -1,12 +1,16 @@
 package com.example.lifeline;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class homeActivity extends AppCompatActivity {
@@ -60,5 +64,33 @@ public class homeActivity extends AppCompatActivity {
                 startActivity(new Intent(homeActivity.this, PaytmActivity.class));
             }
         });
+
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+        Menu menu =bottomNav.getMenu();
+        MenuItem menuItem=menu.getItem(0);
+        menuItem.setChecked(true);
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_profile:
+                    startActivity(new Intent(homeActivity.this,homeActivity.class));
+                    break;
+                case R.id.navigation_appointment:
+                    break;
+                case R.id.navigation_events:
+                    break;
+                case R.id.navigation_leaderboard:
+                    startActivity(new Intent(homeActivity.this,LeaderboardActivity.class));
+                    break;
+
+            }
+           return true;
+        }
+    };
 }
