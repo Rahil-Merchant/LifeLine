fe2267cf87d7bae5ceef270c0d83bd41eab78aac
meranimann <meranimann@gmail.com>
2020-04-02 00:14:08 +0530

    ABC

--- app/src/main/java/com/example/lifeline/AppointmentStepOne.java
+++ app/src/main/java/com/example/lifeline/AppointmentStepOne.java
@@ -7,8 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceControl;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.jaredrummler.materialspinner.MaterialSpinner;
@@ -16,10 +20,17 @@ import com.jaredrummler.materialspinner.MaterialSpinner;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,15 +43,20 @@ import dmax.dialog.SpotsDialog;
 
 public class AppointmentStepOne extends Fragment {
 
+    private SharedViewModel viewModel;
+    private SharedViewModel2 viewModel2;
+    EditText editText;
+
     HorizontalCalendarView calendarView;
     SimpleDateFormat simpleDateFormat;
     public static Calendar selected_date = Calendar.getInstance();
-    public static String string_time = "lul";
+    public String string_time = "lul";
     AlertDialog dialog;
     LocalBroadcastManager localBroadcastManager;
     Unbinder unbinder;
     MaterialSpinner spinner;
     List<String> timing_list = new ArrayList<>();
+    Bundle bundle=new Bundle();
 
     @Override
     public void setRetainInstance(boolean retain) {
@@ -80,7 +96,7 @@ public class AppointmentStepOne extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        timing_list.add("");
+        //timing_list.add("");
         timing_list.add("Morning, 8 AM to 11 AM");
         timing_list.add("Afternoon, 1 PM to 4 PM");
         timing_list.add("Evening, 6 PM to 9 PM");
@@ -91,6 +107,8 @@ public class AppointmentStepOne extends Fragment {
         dialog = new SpotsDialog.Builder().setContext(getContext()).setCancelable(false).build();
         selected_date = Calendar.getInstance();
         selected_date.add(Calendar.DATE,0);
+
+
 
         // LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
 
@@ -114,8 +132,9 @@ public class AppointmentStepOne extends Fragment {
         spinner.setItems(timing_list);
         unbinder = ButterKnife.bind(this,itemView);
         init(itemView);
-        return itemView;
+        //editText = itemView.findViewById(R.id.tv3);
 
+        return itemView;
     }
 
 
@@ -126,6 +145,7 @@ public class AppointmentStepOne extends Fragment {
         localBroadcastManager.sendBroadcast(intent);
     }
 
+    @NonNull
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -133,9 +153,24 @@ public class AppointmentStepOne extends Fragment {
             @Override
             public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
                 string_time = item.toString();
-                //Snackbar.make(view, "Clicked " + string_time, Snackbar.LENGTH_LONG).show();
+
+                AppointmentStepTwo appointmentStepTwo=new AppointmentStepTwo();
+                bundle.putString("time",string_time);
+                //bundle.putString("time2",string_time);
+                appointmentStepTwo.setArguments(bundle);
+                //Snackbar.make(view, "Clicked " + editText, Snackbar.LENGTH_LONG).show();
+
+                viewModel.setText(bundle.getCharSequence("time"));
+                //viewModel.setText(bundle.getCharSequence("time"));
+
+
             }
         });
+        /*AppointmentStepTwo appointmentStepTwo=new AppointmentStepTwo();
+        Bundle bundle=new Bundle();
+        bundle.putString("time","Evening");
+        appointmentStepTwo.setArguments(bundle);*/
+
 
 
     }
@@ -151,14 +186,47 @@ public class AppointmentStepOne extends Fragment {
         horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
             @Override
             public void onDateSelected(Calendar date, int position) {
-               // Snackbar.make(itemView, "Clicked " + selected_date.getTime(), Snackbar.LENGTH_LONG).show();
+                //Snackbar.make(itemView, "Clicked " +date.getTime(), Snackbar.LENGTH_LONG).show();
+                Date date1=date.getTime();
+                Calendar c = Calendar.getInstance();
+                c.setTime(date1);
+                c.add(Calendar.DATE, 1);
+                date1 = c.getTime();
+
+                viewModel2.setText(date1.toString().substring(0,10));
                 if(selected_date.getTimeInMillis() != date.getTimeInMillis())
                 {
                     selected_date = date;
+
                 }
             }
         });
 
+
+
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //viewModel= ViewModelProvider.of(getActivity()).get(SharedViewModel.class);
+        viewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+        viewModel2= ViewModelProviders.of(getActivity()).get(SharedViewModel2.class);
+        viewModel.getText().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+            @Override
+            public void onChanged(CharSequence charSequence) {
+
+                //text1.setText(charSequence);
+            }
+        });
+        viewModel2.getText().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+            @Override
+            public void onChanged(CharSequence charSequence) {
+
+                //text1.setText(charSequence);
+            }
+        });
+    }
+
 }
 
--- app/src/main/java/com/example/lifeline/AppointmentStepTwo.java
+++ app/src/main/java/com/example/lifeline/AppointmentStepTwo.java
@@ -1,21 +1,39 @@
 package com.example.lifeline;
 
 
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
-public class AppointmentStepTwo extends Fragment {
+//import static com.example.lifeline.AppointmentStepOne.string_time;
 
+public class AppointmentStepTwo extends Fragment{
+
+    private SharedViewModel viewModel;
+    private SharedViewModel2 viewModel2;
+    private TextView text1;
+    private TextView text2;
+
+
+    int c=1;
     static AppointmentStepTwo instance;
 
    /* public static AppointmentStepTwo getInstance()
@@ -33,12 +51,11 @@ public class AppointmentStepTwo extends Fragment {
     }
 
 
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        c++;
+        //Toast.makeText(getContext(),"1",Toast.LENGTH_LONG).show();
     }
 
 
@@ -47,6 +64,25 @@ public class AppointmentStepTwo extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View itemView =  inflater.inflate(R.layout.appointment_step_two   ,container,false);
+        text1 = (TextView) itemView.findViewById(R.id.tv2);
+        text2 = (TextView) itemView.findViewById(R.id.tv1);
+
+        Bundle bundle = getArguments();
+
+        if (bundle==null){
+            //String time=bundle.getString("time");
+            text1.setText("Morning,8AM to 11PM");
+        }
+
+        else{
+            String time=bundle.getString("time");
+            //TextView text1 = (TextView) itemView.findViewById(R.id.tv2);
+            text1.setText(time);
+        }
+
+
+
+
         FloatingActionButton fab = (FloatingActionButton)itemView.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,4 +94,26 @@ public class AppointmentStepTwo extends Fragment {
         return itemView;
 
 
-    }}
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //viewModel= ViewModelProvider.of(getActivity()).get(SharedViewModel.class);
+        viewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+        viewModel2= ViewModelProviders.of(getActivity()).get(SharedViewModel2.class);
+        viewModel.getText().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+            @Override
+            public void onChanged(CharSequence charSequence) {
+
+                text1.setText(charSequence);
+            }
+        });
+        viewModel2.getText().observe(getViewLifecycleOwner(), new Observer<CharSequence>() {
+            @Override
+            public void onChanged(CharSequence charSequence) {
+                text2.setText(charSequence);
+            }
+        });
+    }
+}
